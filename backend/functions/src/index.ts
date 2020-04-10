506b5ff86725aa987dd233351e0c25e6df11a6d8
Lucas Mantovani <lucashmantovani@hotmail.com>
2020-04-10 12:18:04 -0300

    Adding messages to schema validation

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -21,17 +21,23 @@ const firestore = app.firestore()
 
 export const createUser = functions.https.onCall(async (data) => {
   const userSchema = Yup.object({
-    email: Yup.string().email().required(),
-    password: Yup.string().required(),
-    name: Yup.string().required(),
-    surname: Yup.string().required(),
-    birthDate: Yup.date().required(),
-    school: Yup.string().required(),
-    gender: Yup.string().oneOf(['male', 'female', 'other']).required(),
+    email: Yup.string()
+      .email('Por favor, digite um email válido')
+      .required('Precisamos do email para permitir o cadastro'),
+    password: Yup.string()
+      .min(6, 'A quantidade mínima de caracteres para senha é 6')
+      .required('Precisamos da senha para permitir o cadastro'),
+    name: Yup.string().required('Gostaríamos de saber seu nome'),
+    surname: Yup.string().required('Gostaríamos de saber seu sobrenome também'),
+    school: Yup.string().required('Gostaríamos de saber o nome da sua escola'),
+    birthDate: Yup.date().required('Gostaríamos de saber quando você nasceu'),
+    gender: Yup.string().oneOf(['male', 'female', 'other']).required('Qual é seu gênero'),
   })
 
   try {
-    userSchema.validateSync(data)
+    userSchema.validateSync(data, {
+      abortEarly: false
+    })
 
     const { email, password, name, surname, birthDate, school, gender } = data
     const user = await admin.auth().createUser({ email, password })
@@ -52,7 +58,7 @@ export const createUser = functions.https.onCall(async (data) => {
       throw new functions.https.HttpsError(
         'invalid-argument',
         'There were invalid arguments. See details for more information',
-        error.errors
+        error.inner
       )
     } else throw new functions.https.HttpsError('already-exists', error.message)
   }
