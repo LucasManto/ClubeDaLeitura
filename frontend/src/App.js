9189c51d309457ab427d48fbcd892ef9d254b531
Lucas Mantovani <lucashmantovani@hotmail.com>
2020-04-29 14:44:19 -0300

    Refactoring App to use Styled Components global style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import './App.css'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from 'react-router-dom'
+
+import GlobalStyle from './styles/global'
 
 import useAuth from './hooks/useAuth'
 
@@ -19,19 +20,23 @@ export default function App() {
   const { user, pending } = useAuth()
 
   return (
-    <Router>
-      <Switch>
-        <Route path="/home">
-          {!pending && !user ? <Redirect to="/" /> : <Home />}
-        </Route>
-        <Route path="/signin">
-          {user ? <Redirect to="/home" /> : <SignIn />}
-        </Route>
-        <Route path="/signup">
-          {user ? <Redirect to="/home" /> : <SignUp />}
-        </Route>
-        <Route path="/">{user ? <Redirect to="/home" /> : <Index />}</Route>
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Switch>
+          <Route path="/home">
+            {!pending && !user ? <Redirect to="/" /> : <Home />}
+          </Route>
+          <Route path="/signin">
+            {user ? <Redirect to="/home" /> : <SignIn />}
+          </Route>
+          <Route path="/signup">
+            {user ? <Redirect to="/home" /> : <SignUp />}
+          </Route>
+          <Route path="/">{user ? <Redirect to="/home" /> : <Index />}</Route>
+        </Switch>
+      </Router>
+
+      <GlobalStyle />
+    </>
   )
 }
